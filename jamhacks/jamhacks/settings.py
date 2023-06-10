82a7ab58d87f017b6d251c36d98e0592b512a5ad
Jason Cameron <jasoncameron.all@gmail.com>
2023-06-10 08:59:08 -0400

    FEAT: Auth0 works!

--- jamhacks/jamhacks/settings.py
+++ jamhacks/jamhacks/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, "webappexample", "templates")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "jamhacks", "../backend/templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
