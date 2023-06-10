4487eae96f90c799b4251064931451a1125e6c94
Jason Cameron <jasoncameron.all@gmail.com>
2023-06-10 09:18:33 -0400

    fmt

--- jamhacks/backend/models.py
+++ jamhacks/backend/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
 # Create your models here.
 
 class User(AbstractUser):
     pass
+
+
 class Badge(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
@@ -20,7 +21,7 @@ class Badge(models.Model):
 class Activity(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
-    ... # todo extend
+    ...  # todo extend
 
     def __str__(self):
         return self.name
