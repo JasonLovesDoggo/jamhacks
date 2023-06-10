5d6defd229dcdaf42cc532293ba74671871e4551
Jason Cameron <jasoncameron.all@gmail.com>
2023-06-10 02:03:27 -0400

    sus itr was me not joshua

--- backgud/backgud/settings.py
+++ backgud/backgud/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'backgud.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject .com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
