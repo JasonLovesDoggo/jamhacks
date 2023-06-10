4493185fe57fb632288e56a0442070024df8f640
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2023-06-10 01:59:34 -0400

    django worki

---
+++ TODO.md
@@ -0,0 +1,6 @@
+- auth system
+- openCV system
+- form DB
+### WANTS
+- badges system
+- leaderboard system
---
+++ backfixer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BackfixerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backfixer'
