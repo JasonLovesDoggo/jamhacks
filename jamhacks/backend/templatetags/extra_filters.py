a6f6c264f4eb827bb2acd82b6f9b08af53cdd8f1
Jason Cameron <jasoncameron.all@gmail.com>
2023-06-10 23:08:59 -0400

    changes

--- jamhacks/backend/templatetags/extra_filters.py
+++ jamhacks/backend/templatetags/extra_filters.py
@@ -4,3 +4,20 @@ register = template.Library()
 @register.filter(name='addcss')
 def addcss(field, css):
     return field.as_widget(attrs={"class":css})
+
+
+@register.filter
+def divide(value, arg):
+    try:
+        return int(value) // int(arg)
+    except (ValueError, ZeroDivisionError):
+        return None
+
+@register.filter
+def mod(value, arg):
+    try:
+        return int(value) % int(arg)
+    except (ValueError, ZeroDivisionError):
+        return None
+
+
